dc4a4bc79ef7755ea3eb74a7e8f198b70a235634
osama shehta <osama.shehtafathi@gmail.com>
2025-09-30 23:40:16 +0300

    feat add suspense for toolbar component

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import Search from "./Search";
 import Filter from "./Filter";
 import Container from "../shared/Container";
@@ -9,14 +9,16 @@ const Toolbar = () => {
   const router = useRouter();
   return (
     <Container className="my-10 flex flex-wrap   items-center justify-center gap-4">
-      <Search />
-      <Filter />
-      <button
-        onClick={() => router.push("/")}
-        className=" px-2 py-1 bg-red-500 text-white rounded-[10px] hover:bg-red-600"
-      >
-        clear All
-      </button>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Search />
+        <Filter />
+        <button
+          onClick={() => router.push("/")}
+          className=" px-2 py-1 bg-red-500 text-white rounded-[10px] hover:bg-red-600"
+        >
+          clear All
+        </button>
+      </Suspense>
     </Container>
   );
 };
