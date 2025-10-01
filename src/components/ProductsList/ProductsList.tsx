0b034d98487a38e0be44e0ab572343397a9ef41e
osama shehta <osama.shehtafathi@gmail.com>
2025-10-01 20:57:38 +0300

    feat: update ProductsList component layout and add link to product details

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,5 +1,6 @@
 import { Product } from "@/types/types";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 interface ProductsListProps {
   products: Product[];
@@ -10,7 +11,7 @@ const ProductsList = ({ products }: ProductsListProps) => {
       {products.map((product) => (
         <div
           key={product.id}
-          className="border border-gray-300 p-4 flex flex-col items-center justify-center rounded-[10px] shadow-[0px_0px_14px_#00000025]"
+          className="border border-gray-300 p-2 flex flex-col items-center justify-center rounded-[10px] shadow-[0px_0px_14px_#00000025]"
         >
           <Image
             src={product.thumbnail}
@@ -19,12 +20,18 @@ const ProductsList = ({ products }: ProductsListProps) => {
             height={200}
             className="object-cover mb-4"
           />
-          <div className="flex items-center justify-between w-full gap-2">
-            <h2 className="text-[16px] font-semibold mb-2">{product.title}</h2>
-            <p className="text-lg font-semibold text-green-600">
+          <div className="flex-grow flex items-center justify-between w-full gap-2">
+            <h2 className="text-[14px] font-semibold mb-2">{product.title}</h2>
+            <p className="text-[12px] font-semibold text-green-600">
               ${product.price}
             </p>
           </div>
+          <Link
+            href={`/products/${product.id}`}
+            className="bg-blue-500 hover:bg-blue-600 text-white text-sm font-semibold py-1 px-2 rounded-[10px]"
+          >
+            View Details
+          </Link>
         </div>
       ))}
     </div>
